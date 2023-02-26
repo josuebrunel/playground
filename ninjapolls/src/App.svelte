0dfb315f2faaaf19b4ac2a33dc349b5bc4c53bf8
Josue Kouka <josuebrunel@gmail.com>
2023-02-26 12:53:08 -0800

    ninjapolls: add header and footer components

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting
+		industry. Lorem Ipsum has been the industry's standard dummy text ever
+		since the 1500s, when an unknown printer took a galley of type and
+		scrambled it to make a type specimen book. It has survived not only five
+		centuries, but also the leap into electronic typesetting, remaining
+		essentially unchanged. It was popularised in the 1960s with the release
+		of Letraset sheets containing Lorem Ipsum passages, and more recently
+		with desktop publishing software like Aldus PageMaker including versions
+		of Lorem Ipsum.
+	</p>
 </main>
+
+<Footer />
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		max-width: 960px;
+		margin: 40px auto;
 	}
 </style>
