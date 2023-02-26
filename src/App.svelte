1b0bc1ffd77a1b7622113b87d5f78ae2f6d84479
Josue Kouka <josuebrunel@gmail.com>
2023-02-25 17:28:10 -0800

    first commit: inline function

---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+	let firstName = "Josh";
+	let lastName = "Loking";
+
+	$: fullName = `${firstName} ${lastName}`;
+	$: {
+		console.log(firstName, lastName);
+	}
+
+	const handleClick = () => {
+		fullName = "Yosuke Loking";
+	};
+
+	let people = [
+		{ name: "josh", age: 25, id: 1 },
+		{ name: "kimia", age: 23, id: 2 },
+		{ name: "yosuke", age: 25, id: 3 },
+	];
+
+	const deletePerson = (id) => {
+		console.log("deleting person ", id);
+		people = people.filter((person) => person.id != id);
+	};
+</script>
+
+<main>
+	<h1>Hello {fullName}!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<button on:click={handleClick}> update name</button>
+	<input type="text" bind:value={firstName} />
+	<input type="text" bind:value={lastName} />
+
+	<div>
+		{#each people as person (person.id)}
+			<div>
+				<h4>{person.name}</h4>
+				<p>{person.age} years old</p>
+				<button on:click={() => deletePerson(person.id)}>delete</button>
+			</div>
+		{:else}
+			<p>There are no people to show</p>
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
