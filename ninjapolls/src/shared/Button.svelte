14273c9537e182c06ff8a7a79485ec8b9327543b
Josue Kouka <josuebrunel@gmail.com>
2023-02-26 17:42:25 -0800

    add custom button component

---
+++ ninjapolls/src/shared/Button.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let type = "primary";
+    export let flat = false;
+    export let inverse = false;
+</script>
+
+<button class={type} class:flat class:inverse>
+    <slot />
+</button>
+
+<style>
+    button {
+        border: 0;
+        cursor: pointer;
+        border-radius: 6px;
+        padding: 8px 12px;
+        font-weight: bold;
+        box-shadow: 1px 2px 3px rgb(0, 0, 0, 0);
+    }
+    .primary {
+        background: #d91b42;
+        color: white;
+    }
+
+    .secondary {
+        background: #456496;
+        color: white;
+    }
+
+    .flat {
+        box-shadow: none;
+    }
+
+    .primary.inverse {
+        color: #d91b42;
+        background: white;
+    }
+
+    .secondary.inverse {
+        color: #456496;
+        background: white;
+    }
+</style>
